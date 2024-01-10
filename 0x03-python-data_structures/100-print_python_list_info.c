e0a08af86732f4f42120b3b89497288fa0d11d28
TosBam <samuelson.os19@gmail.com>
2024-01-09 20:52:10 -0800

    what is actually happening here

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -3,10 +3,10 @@
 
 /**
  * print_python_list_info - prints python list info
- *
  * @p: PyObject
  * Return: no return
  */
+
 void print_python_list_info(PyObject *p)
 {
 	long int size, i;
@@ -19,7 +19,7 @@ void print_python_list_info(PyObject *p)
 	list = (PyListObject *)p;
 	printf("[*] Allocated = %ld\n", list->allocated);
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size; i++);
 	{
 		item = PyList_GetItem(p, i);
 		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
